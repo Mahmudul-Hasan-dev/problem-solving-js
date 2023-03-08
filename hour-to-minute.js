7b48e3f7e2fa65642297ec8602e2fe915851da55
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-08 18:57:33 +0600

    hour to minute completed

--- hour-to-minute.js
+++ hour-to-minute.js
@@ -1,8 +1,10 @@
+//declare function
 function hourToMinute(hour) {
     var minute = hour * 60;
     return minute;
 }
 
 var myHour = 24;
+//call function and send variable to function as parameter
 var myHourMinute = hourToMinute(myHour);
 console.log(myHourMinute);
