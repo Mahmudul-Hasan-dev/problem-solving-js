be20b20ea5a5d67ce64e1634d5a87730ecfaafd9
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-08 18:45:09 +0600

    comment done

--- leap-year-output.js
+++ leap-year-output.js
@@ -1,14 +1,19 @@
 function leapYear(year) {
+    //take an empty array to store the value of new array
     var leapYearArr = [];
     for (i = 0; i <= year.length; i++) {
         if (year[i] % 4 == 0) {
             leapYearArr.push(year[i]);
         }
     }
+
+    //return array
     return leapYearArr;
 
 }
 
-var years = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2026, 2027, 2028]
+var years = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2026, 2027, 2028];
+
+//call function and send input value as parameter
 var result = leapYear(years);
 console.log(result);
